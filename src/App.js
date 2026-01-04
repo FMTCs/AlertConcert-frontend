8f3b27d4ed1bb41d543f78beefe2abc87580d17f
김민제 <loloy@Kovalt03.local>
2026-01-04 14:29:27 +0900

    figma upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { LoginScreen } from "./Login";
+import { SignupScreen } from "./app/components/SignupScreen";
+import { ConcertRecommendations } from "./app/components/ConcertRecommendations";
 import './App.css';
 
 function App() {
+const [currentScreen, setCurrentScreen] = useState<AppScreen>("login");
+
+  const handleLogin = () => {
+    setCurrentScreen("recommendations");
+  };
+
+  const handleGoToSignup = () => {
+    setCurrentScreen("signup");
+  };
+
+  const handleGoToLogin = () => {
+    setCurrentScreen("login");
+  };
+
+  const handleSignupComplete = () => {
+    setCurrentScreen("recommendations");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="size-full">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/signup" element={<SignupScreen />} />
+
+          <Route
+            path="/recommendations"
+            element={<ConcertRecommendations />}
+          />
+
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
